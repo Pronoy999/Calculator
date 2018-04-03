e88e09e72821a42a20a8757a597354d264339591
Pronoy Mukherjee <pronoy mukherjee>
2018-04-03 20:06:50 +0530

    Added a clear and delete button.

--- app/src/main/java/com/pronoymukherjee/calculator_beta1/MainActivity.java
+++ app/src/main/java/com/pronoymukherjee/calculator_beta1/MainActivity.java
@@ -1,36 +1,46 @@
 package com.pronoymukherjee.calculator_beta1;
 
+import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.content.Intent;
 import android.support.annotation.IntegerRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.AppCompatButton;
+import android.support.v7.widget.AppCompatEditText;
+import android.util.Log;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    EditText e;
+    AppCompatEditText e;
     TextView ans;
     TextView mem;
     String m = "";
-    Button sol, memR, memA;
+    AppCompatButton sol, memR, memA,clearButton,deleteButton;
     char prec[] = {'-', '+', '*', '/', '^'};
     char stack[];
     int top = -1;
     double stack1[];
-
+    View view;
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        e = (EditText) findViewById(R.id.e);
+        e = (AppCompatEditText) findViewById(R.id.e);
         ans = (TextView) findViewById(R.id.ans);
         mem = (TextView) findViewById(R.id.mem);
-        sol = (Button) findViewById(R.id.ansButton);
-        memR = (Button) findViewById(R.id.mem1);
-        memA = (Button) findViewById(R.id.mem2);
+        sol = (AppCompatButton) findViewById(R.id.ansButton);
+        memR = (AppCompatButton) findViewById(R.id.mem1);
+        memA = (AppCompatButton) findViewById(R.id.mem2);
+        clearButton= (AppCompatButton) findViewById(R.id.clearButton);
+        deleteButton= (AppCompatButton) findViewById(R.id.delete);
+        view=MainActivity.this.getCurrentFocus();
         sol.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
             @Override
             public void onClick(View v) {
                 try {
@@ -45,6 +55,16 @@ public class MainActivity extends AppCompatActivity {
                     e.setText(a1);
                     m = "";
                     m = a1;
+                    InputMethodManager inputMethodManager= (InputMethodManager)
+                            getSystemService(Activity.INPUT_METHOD_SERVICE);
+                    try {
+                        if(view==null)
+                            view=MainActivity.this.getCurrentFocus();
+                        inputMethodManager.hideSoftInputFromInputMethod(view.getWindowToken(), 0);
+                    }
+                    catch (NullPointerException e){
+                        Log.d(MainActivity.class.getSimpleName(),e.toString());
+                    }
                 } catch (Exception ignored) {
                     ans.setText("Inavlid Expression");
                     startActivity(new Intent(MainActivity.this, MainActivity.class));
@@ -65,6 +85,20 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 m = e.getText().toString();
                 mem.setText(m);
+            }
+        });
+        clearButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                e.getText().clear();
+            }
+        });
+        deleteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String temp=e.getText().toString();
+                temp=temp.substring(0,temp.length()-1);
+                e.setText(temp);
             }
         });
     }
